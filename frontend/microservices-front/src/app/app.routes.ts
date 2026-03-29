240d987f559b2248b04e11b4852fa4d44dffd39f
WeTi12 <wetik12@gmail.com>
2026-03-29 17:02:33 +0200

    panel administracyjny

--- frontend/microservices-front/src/app/app.routes.ts
+++ frontend/microservices-front/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'login' },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
 	{ path: 'home', component: HomeComponent },
+	{ path: 'admin', component: AdminPanelComponent },
 	{ path: '**', redirectTo: 'login' }
 ];
