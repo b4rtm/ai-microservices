69e2f2d9ade6e390c8bf7cca5735cee1bd3f83a6
Daniel Chróst <danielchrost165@gmail.com>
2026-04-17 13:16:07 +0200

    spam detector load balancing added

--- spam-fast-api/app/consul.py
+++ spam-fast-api/app/consul.py
@@ -1,32 +1,37 @@
 import httpx
+import os
 
 from app.config import (
     CONSUL_URL,
-    SERVICE_ADDRESS,
-    SERVICE_ID,
     SERVICE_NAME,
     SERVICE_PORT,
 )
 
+def get_service_id():
+    return f"{SERVICE_NAME}-{os.getenv('HOSTNAME')}"
 
 async def register() -> None:
+    # Każdy kontener ma unikalny HOSTNAME → używamy go jako adresu
+    address = os.getenv("HOSTNAME")
+
     async with httpx.AsyncClient() as client:
         await client.put(
             f"{CONSUL_URL}/agent/service/register",
             json={
-                "ID": SERVICE_ID,
+                "ID": get_service_id(),
                 "Name": SERVICE_NAME,
-                "Address": SERVICE_ADDRESS,
+                "Address": address,                     # 🔥 KLUCZOWE
                 "Port": SERVICE_PORT,
                 "Check": {
-                    "HTTP": f"http://{SERVICE_ADDRESS}:{SERVICE_PORT}/health",
+                    "HTTP": f"http://{address}:{SERVICE_PORT}/health",
                     "Interval": "10s",
                     "DeregisterCriticalServiceAfter": "1m",
                 },
             },
         )
 
-
 async def deregister() -> None:
     async with httpx.AsyncClient() as client:
-        await client.put(f"{CONSUL_URL}/agent/service/deregister/{SERVICE_ID}")
+        await client.put(
+            f"{CONSUL_URL}/agent/service/deregister/{get_service_id()}"
+        )
